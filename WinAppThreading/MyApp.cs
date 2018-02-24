7e74d8760041ff605de8a217c05feac8addd9f3f
Krzysztof Szczurowski <kszczurowski@absolute.com>
2018-02-24 00:16:29 -0800

    Fixing item's namings and preparing form for new components;

--- WinAppThreading/MyApp.cs
+++ WinAppThreading/MyApp.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace WinAppThreading
 {
-    static class Program
+    static class MyApp
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +16,7 @@ namespace WinAppThreading
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }

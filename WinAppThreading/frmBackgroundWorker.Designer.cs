a6e27133dbb6b460c9b5dea4b571b0ed755833ed
Krzysztof Szczurowski <kszczurowski@absolute.com>
2018-02-24 13:32:20 -0800

    Changed form name as required; added existing method GetDataHelper to BLL.Logic project; Implemented required and necessary methods to show BackgroundWorker workings;

---
+++ WinAppThreading/frmBackgroundWorker.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WinAppThreading
+{
+    partial class frmBackgroundWorker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.dataGridViewCities = new System.Windows.Forms.DataGridView();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCities)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bgw
+            // 
+            this.bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_DoWork);
+            this.bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw_RunWorkerCompleted);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar,
+            this.toolStripLabel,
+            this.toolStripTime});
+            this.statusStrip.Location = new System.Drawing.Point(0, 359);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(527, 25);
+            this.statusStrip.TabIndex = 0;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // dataGridViewCities
+            // 
+            this.dataGridViewCities.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCities.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewCities.Name = "dataGridViewCities";
+            this.dataGridViewCities.RowTemplate.Height = 24;
+            this.dataGridViewCities.Size = new System.Drawing.Size(503, 344);
+            this.dataGridViewCities.TabIndex = 1;
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(100, 19);
+            // 
+            // toolStripLabel
+            // 
+            this.toolStripLabel.Name = "toolStripLabel";
+            this.toolStripLabel.Size = new System.Drawing.Size(122, 20);
+            this.toolStripLabel.Text = "<Process Status>";
+            // 
+            // toolStripTime
+            // 
+            this.toolStripTime.Name = "toolStripTime";
+            this.toolStripTime.Size = new System.Drawing.Size(62, 20);
+            this.toolStripTime.Text = "<Time>";
+            // 
+            // frmBackgroundWorker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 384);
+            this.Controls.Add(this.dataGridViewCities);
+            this.Controls.Add(this.statusStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmBackgroundWorker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "COMP3618 Week7 Lab2 Krzysztof Szczurowski";
+            this.Activated += new System.EventHandler(this.frmBackgroundWorker_Activated);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCities)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgw;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripLabel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripTime;
+        private System.Windows.Forms.DataGridView dataGridViewCities;
+    }
+}
+
---
+++ WinAppThreading/frmBackgroundWorker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL.Logic;
+
+namespace WinAppThreading
+{
+    public partial class frmBackgroundWorker : Form
+    {
+        public AppLogic Helper { get; private set; }
+        DateTime startDateTime = DateTime.Now;
+
+        public frmBackgroundWorker()
+        {
+            InitializeComponent();
+            Helper = new AppLogic();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{Helper.GetTester("Kris")}", "Title", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void frmBackgroundWorker_Activated(object sender, EventArgs e)
+        {
+            bgw.RunWorkerAsync();
+            timer.Start();
+        }
+
+        private void bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DataTable dt;
+            toolStripLabel.Text = "Loading ... " + "Thanks for your patience";
+            dt = GetDataTable(1000000);
+            e.Result = dt;
+            toolStripLabel.Text = "Please, wait ...";
+        }
+
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            toolStripProgressBar.Value = 100;
+            dataGridViewCities.DataSource = e.Result;
+            toolStripLabel.Text = "";
+            toolStripProgressBar.Value = 0;
+            timer.Stop();
+            toolStripTime.Text = "";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = DateTime.Now.Subtract(startDateTime);
+            string sTime = $"  ... {ts.Minutes.ToString("00")} : {ts.Seconds.ToString("00")} : {ts.Milliseconds.ToString("000")}";
+
+            toolStripTime.Text = sTime;
+            if (toolStripProgressBar.Value == toolStripProgressBar.Maximum)
+            {
+                toolStripProgressBar.Value = 0;
+            }
+            toolStripProgressBar.PerformStep();
+        }
+
+        #region Private methods
+
+        private DataTable GetDataTable(int Rows)
+        {
+            GetDataHelper getData = new GetDataHelper();
+            return (getData.GetDataSetCities(Rows).Tables[0]);
+        }
+
+        #endregion
+
+    }
+}
